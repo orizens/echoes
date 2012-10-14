a87b86c62c2ce7c2cc79c410e20f5f09d31f83eb
orizens <farhioren@gmail.com>
2012-10-14 18:32:44 +0200

    final updated

--- js/script.js
+++ js/script.js
@@ -3,7 +3,18 @@ var Echoes = {
 	Models: {},
 	Views: {},
 	Collections: {},
-	Templates: {},
+	Templates: {
+		load: function(templatesUrl, callback){
+			//- load templates and then start player
+			templatesUrl = templatesUrl ? templatesUrl : 'templates/templates.html';
+			$.get(templatesUrl, function(templates){
+				$('body').append(templates);
+				if (callback) {
+					callback();
+				}
+			});
+		}
+	},
 	Utils: {
 		formatNumberWithComma: function(num) {
 			return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
@@ -352,14 +363,3 @@ Echoes.Player = Backbone.Router.extend({
 		this.navigate('explore', {trigger: true});
 	}
 });
-
-Echoes.Templates.load = function(templatesUrl, callback){
-	//- load templates and then start player
-	templatesUrl = templatesUrl ? templatesUrl : 'templates/templates.html';
-	$.get(templatesUrl, function(templates){
-		$('body').append(templates);
-		if (callback) {
-			callback();
-		}
-	});
-};
