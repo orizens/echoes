b39d22bd5460367015bca5f9d072e2bd99fae22e
orizens <farhioren@gmail.com>
2012-12-02 09:09:25 +0200

    fixed bug in media info , player component

--- js/models/youtube_item_info.js
+++ js/models/youtube_item_info.js
@@ -22,7 +22,7 @@ define([
 		},
 
 		parse: function(response) {
-			return response.data.items.length ? response.data.items[0] : {};
+			return response.data.items && response.data.items.length ? response.data.items[0] : {};
 		}
 	});
    
