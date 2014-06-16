cbaed033a46724224eeb29cfe98562adda058cc2
orizens <farhioren@gmail.com>
2014-06-16 15:37:42 +0300

    updated description of update

--- public/js/models/updates-service.js
+++ public/js/models/updates-service.js
@@ -11,7 +11,7 @@ define([
 			// format: yyyymmdd(-hhmm)
 			// (hhmm) is optional
 			version: '201406161531',
-			description: 'fixed issues with updates center.',
+			description: '',
 			manualCheck: false
 		},
 
