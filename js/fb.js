9c8b58e720c18f4a2b791c1d6674101f60771d4a
orizens <farhioren@gmail.com>
2014-06-19 08:26:47 +0300

    fixed #43

--- js/fb.js
+++ js/fb.js
@@ -1,7 +1,9 @@
-define(['facebook'], function(){
-	FB.init({
-		appId: '277907182339554',
-	});
+define(['facebook'], function(FB){
+	if (FB){
+		FB.init({
+			appId: '277907182339554',
+		});
+	}
 	// FB.getLoginStatus(function(response) {
 	// 	console.log(response);
 	// });
