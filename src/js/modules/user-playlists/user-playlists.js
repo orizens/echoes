a2a14cd5033d4baf29bb29ba8a44f685cc4d1ba2
orizens <farhioren@gmail.com>
2014-09-20 17:48:18 +0300

    user playlists: adding filter

--- src/js/modules/user-playlists/user-playlists.js
+++ src/js/modules/user-playlists/user-playlists.js
@@ -1,6 +1,7 @@
 var _ = require('underscore');
 var Backbone = require('backbonejs');
 var YoutubeItemView = require('../../views/youtube_user_playlist_item.js');
+var PlaylistsFilter = require('./user-playlists-filter');
 
 module.exports = Timber.module('View', {
 
@@ -18,6 +19,8 @@ module.exports = Timber.module('View', {
 		}
 	},
 	initialize: function(){
+		this.filter = PlaylistsFilter.create(this.model);
+		this.listenTo(this.filter, 'change', this.filterList);
 		// this.listenTo(Backbone, 'user:authorized', this.handleAuthorize);
 		this.listenTo(this.model.youtube.profile, 'change:items', this.list);
 	},
@@ -25,7 +28,13 @@ module.exports = Timber.module('View', {
 	// handleAuthorize: function(authResult){
 	// 	this.model.youtube.profile.connect();
 	// },
-
+	filterList: function(val){
+		var playlists = this.model.youtube.playlists;
+		this.collection = playlists.filter(function(item){
+			return item.attributes.snippet.title.toLowerCase().indexOf(val.toLowerCase()) > -1;
+		});
+		this.render();
+	},
 	list: function(){
 		this.model.youtube.updateUserPlaylists();
 	},
