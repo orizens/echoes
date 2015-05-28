2d55bd79a491bc22bd13eb01623e011a32772f74
orizens <farhioren@gmail.com>
2015-05-28 13:58:29 +0300

    added social share

--- gulp/style.js
+++ gulp/style.js
@@ -2,7 +2,8 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var concat = require('gulp-concat');
 var sourcemaps = require('gulp-sourcemaps');
-// var uncss = require('gulp-uncss');
+var uncss = require('gulp-uncss');
+// var csso = require('gulp-csso');
 // var glob = require('glob');
 
 module.exports = gulp.task('style', function () {
@@ -12,11 +13,14 @@ module.exports = gulp.task('style', function () {
   	])
     .pipe(sourcemaps.init())
     .pipe(less())
+    // .pipe(csso())
     .pipe(concat('style.css'))
     .pipe(sourcemaps.write())
     // .pipe(uncss({
-    //     html: glob.sync('./src/app/**/*.html').concat('./src/index.html'),
-    //     ignore: [/^(\.show\-description)+.*|(fa).*|(active)|(\.open)/]
+    //     html: [ './src/app/**/*.html', './src/index.html' ],
+    //     ignore: [ '.active', 'show-description', 'fa-*', 'user-signed-in',
+    //               'show-youtube-player', 'fullscreen', 'yt-playing', 
+    //               'transition-in', '[drawer-opened]*']
     // }))
     .pipe(gulp.dest('.tmp'));
 });
