fafb76d8c2b35e8304b7c0e4cd18a23980995bb3
orizens <farhioren@gmail.com>
2015-06-07 12:31:29 +0300

    trying protractor local machine on browserstack

---
+++ gulp/config/protractor.conf.bs.js
@@ -0,0 +1,23 @@
+exports.config = {
+  capabilities: {
+    'browserstack.user' : process.env.bs_user,
+    'browserstack.key' : process.env.bs_key,
+
+    // Needed for testing localhost
+    'browserstack.local' : 'true',
+    'browserstack.debug': 'true',
+
+    // Settings for the browser you want to test
+    'browserName' : 'Chrome',
+    'browser_version' : '36.0',
+    'os' : 'OS X',
+    'os_version' : 'Mavericks',
+    'resolution' : '1024x768'
+  },
+  
+  // Browserstack's selenium server address
+  seleniumAddress: 'http://hub.browserstack.com/wd/hub',
+
+  // Pattern for finding spec files
+  specs: ['../../tests/e2e/**/*spec.js']
+}
