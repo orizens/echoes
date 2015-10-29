2fbb70dcc7b6fc901b4ca92f6cf58d5812813c1b
orizens <farhioren@gmail.com>
2015-10-29 11:34:54 +0200

    updated playlist editor

--- src/app/playlist-editor/playlist.editor.drv.js
+++ src/app/playlist-editor/playlist.editor.drv.js
@@ -6,14 +6,14 @@
         .directive('playlistEditor', playlistEditor);
 
     /* @ngInject */
-    function playlistEditor (PlaylistEditorSettings, UserPlaylists) {
+    function playlistEditor (PlaylistEditorSettings) {
         // Usage:
         //	<playlist-editor></playlist-editor>
         // Creates:
         //
         var directive = {
             link: link,
-            controller: controller,
+            controller: 'PlaylistEditorCtrl',
             controllerAs: 'vm',
             restrict: 'E',
             replace: true,
@@ -21,57 +21,6 @@
             scope: {}
         };
         return directive;
-
-        /* @ngInject */
-        function controller ($scope) {
-            /*jshint validthis: true */
-        	var vm = this;
-        	vm.getMedia = PlaylistEditorSettings.getMedia;
-        	vm.playlists = UserPlaylists.tracks;
-        	vm.search = '';
-        	vm.add = add;
-        	vm.showCreate = false;
-            vm.isPlaylistNameExists = isPlaylistNameExists;
-        	vm.create = create;
-        	vm.remove = remove;
-
-        	// activate();
-
-        	// function activate () {}
-
-        	function add (playlist) {
-        		playlist.inProcess = true;
-        		UserPlaylists.addToPlaylist(playlist.id, vm.getMedia()).then(showAddResult);
-	        	
-	        	function showAddResult (response) {
-	        		if (response.status === 200) {
-	        			playlist.contentDetails.itemCount++;
-	        			playlist.inProcess = false;
-	        			$scope.$apply();
-	        		}
-	        	}
-        	}
-
-        	function create () {
-        		// TODO - add description
-        		UserPlaylists.createPlaylist(vm.search, '');
-        	}
-
-        	function remove (playlist) {
-        		playlist.inProcess = true;
-        		return UserPlaylists.removePlaylist(playlist.id).then( (response) => {
-        			playlist.inProcess = false;
-        			$scope.$apply();
-        		});
-        	}
-
-            function isPlaylistNameExists () {
-                var allTitles = vm.playlists.map((playlist) => {
-                    return playlist.snippet.title.toLowerCase();
-                }).join(' ');
-                vm.showCreate = allTitles.indexOf(vm.search) === -1;
-            }
-        }
 
         function link(scope, element, attrs) {
         	var $modal = element;
