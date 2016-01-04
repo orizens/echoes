ce43da1934d9fe406676184c8698fe56f7834f92
orizens <farhioren@gmail.com>
2016-01-04 11:18:11 +0200

    #84 - fixed now playlist - typo in importing component

--- src/components/now-playing/now-playlist/index.js
+++ src/components/now-playing/now-playlist/index.js
@@ -1,6 +1,6 @@
 import angular from 'angular';
 import AngularSortableView from 'angular-sortable-view/src/angular-sortable-view.js';
-import NowPlaylistComponent from './now-playlist.component';
+import { NowPlaylistComponent } from './now-playlist.component';
 
 export default angular.module('now-playlist', [
 	    'app.core',
