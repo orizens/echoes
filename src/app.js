98841b4336392b15f6179acbfcb413ce3166c3ad
orizens <farhioren@gmail.com>
2016-01-13 18:32:39 +0200

    #84 - added eslint, removed jslint, fixes errors of eslint

--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ import ngRoute from 'angular-router-browserify';
 import AngularAnimate from 'angular-animate';
 import AngularSanitize from 'angular-sanitize';
 import AngularBootstrap from 'angular-ui-bootstrap';
-
+/*eslint-disable */
 import LocalStorageModule from 'angular-local-storage';
+/*eslint-enable */
 import AppCore from './core';
 
 import YoutubeVideos from './components/youtube-videos';
@@ -18,66 +19,52 @@ import Drawer from './components/drawer';
 import PlaylistEditor from './components/playlist-editor';
 
 ngRoute(angular);
+angular.module('echoes', [
+	// framework wide components
+	'ngRoute',
+	AngularAnimate,
+	AngularSanitize,
+	AngularBootstrap,
 
-	angular.module('echoes', [
-		// framework wide components
-		'ngRoute',
-		AngularAnimate,
-		AngularSanitize,
-		AngularBootstrap,
+	// services
+	'LocalStorageModule',
+	AppCore.name,
 
-		// services
-		'LocalStorageModule',
-		AppCore.name,
+	// ui-components
+	YoutubeVideos.name,
+	Loader.name,
+	SearchPanel.name,
+	NowPlaying.name,
+	YoutubePlayer.name,
+	UserProfile.name,
+	Drawer.name,
+	PlaylistEditor.name
+	// '720kb.socialshare',
+])
+.config(config);
 
-    	// ui-components
-    	YoutubeVideos.name,
-    	Loader.name,
-    	SearchPanel.name,
-    	NowPlaying.name,
-    	YoutubePlayer.name,
-    	UserProfile.name,
-    	Drawer.name,
-    	PlaylistEditor.name
-		// 'youtube.playlists',
-		// 'youtube.player',
-		// 'media.info',
-		// 'drawer',
-		// 'presets',
-		// 'infinite-scroll',
-		// 'navigator',
-		// 'playlist.editor',
-		// 'playlist.saver',
-		// 'angular-sortable-view',
-		// '720kb.socialshare',
-		// 'google.api.loader',
-		// 'google-signin',
-		// 'user-profile'
-	])
-	.config(config);
+/* @ngInject */
+function config ($routeProvider, localStorageServiceProvider, GapiApiSetterProvider) {
+	GapiApiSetterProvider.config({
+		scope: 'youtube',
+		api: {
+			client: 'youtube',
+			version: 'v3'
+		},
+		clientId: '971861197531'
+	});
 
-	/* @ngInject */
-	function config ($routeProvider, localStorageServiceProvider, GapiApiSetterProvider) {
-		GapiApiSetterProvider.config({
-			scope: 'youtube',
-			api: {
-				client: 'youtube',
-				version: 'v3'
-			},
-			clientId: '971861197531'
+	localStorageServiceProvider.setPrefix('EchoesPlayer');
+
+	$routeProvider
+
+		// .when('/video/:id', {
+		// 	templateUrl: 'app/youtube-video/youtube.video.tpl.html',
+		// 	controller: 'YoutubeVideoCtrl',
+		// 	controllerAs: 'vm',
+		// })
+
+		.otherwise({
+			redirectTo: '/'
 		});
-
-		localStorageServiceProvider.setPrefix('EchoesPlayer');
-
-		$routeProvider
-
-			// .when('/video/:id', {
-			// 	templateUrl: 'app/youtube-video/youtube.video.tpl.html',
-			// 	controller: 'YoutubeVideoCtrl',
-			// 	controllerAs: 'vm',
-			// })
-
-			.otherwise({
-				redirectTo: '/'
-			});
-	}
+}
