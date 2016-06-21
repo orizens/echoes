3bbc0bbdbd40f4aa3dae4d7c3d83c6d3ba33b8f0
Oren Farhi <farhioren@gmail.com>
2016-06-21 10:16:57 +0300

    updated travis to use deploy after_success script

--- gulp/deploy.sh
+++ gulp/deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-git config --global user.email "farhioren@gmail.com"
-git config --global user.name "travis-ci"
-npm run release
-cd dist
-git init
-git add .
-git commit -m "deployed new version from travis"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  git config --global user.email "farhioren@gmail.com"
+  git config --global user.name "travis-ci"
+  npm run release
+  cd dist
+  git init
+  git add .
+  git commit -m "deployed new version from travis"
+  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+fi
