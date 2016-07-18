106f811ce3e4f450326d873bc8e3ed1294bc9157
Oren Farhi <farhioren@gmail.com>
2016-07-18 12:20:02 +0300

    udpated lint defs

--- src/components/youtube-videos/youtube-videos.component.js
+++ src/components/youtube-videos/youtube-videos.component.js
@@ -4,58 +4,58 @@ import template from './youtube-videos.tpl.html';
 // Usage:
 //  <youtube-videos></youtube-videos>
 export let YoutubeVideosComponent = {
-	templateUrl: template,
-	selector: 'youtubeVideos',
-	controllerAs: 'youtubeVideos',
-	scope: {},
-	bindToController: true,
-	// replace: true,
-	restrict: 'E',
-	/* @ngInject */
-	controller: class YoutubeVideosCtrl {
-		/* @ngInject */
-		constructor(YoutubePlayerSettings, YoutubeSearch, PlaylistEditorSettings,
-			PlaylistInfo, $state) {
-			Object.assign(this, {
-				YoutubePlayerSettings, YoutubeSearch, PlaylistInfo,
-				PlaylistEditorSettings, $state
-			});
+  templateUrl: template,
+  selector: 'youtubeVideos',
+  controllerAs: 'youtubeVideos',
+  scope: {},
+  bindToController: true,
+  // replace: true,
+  restrict: 'E',
+  /* @ngInject */
+  controller: class YoutubeVideosCtrl {
+    /* @ngInject */
+    constructor(YoutubePlayerSettings, YoutubeSearch, PlaylistEditorSettings,
+      PlaylistInfo, $state) {
+      Object.assign(this, {
+        YoutubePlayerSettings, YoutubeSearch, PlaylistInfo,
+        PlaylistEditorSettings, $state
+      });
 
-			this.getFeedType = YoutubeSearch.getFeedType;
-			this.videos = YoutubeSearch.items;
-			this.searchMore = YoutubeSearch.searchMore;
-			this.playlistBackState = 'videos';
-			this.hideFilters = true;
+      this.getFeedType = YoutubeSearch.getFeedType;
+      this.videos = YoutubeSearch.items;
+      this.searchMore = YoutubeSearch.searchMore;
+      this.playlistBackState = 'videos';
+      this.hideFilters = true;
 
-			YoutubeSearch.resetPageToken();
-			if (!this.videos.length) {
-				YoutubeSearch.search();
-			}
-		}
+      YoutubeSearch.resetPageToken();
+      if (!this.videos.length) {
+        YoutubeSearch.search();
+      }
+    }
 
-		playVideo(video) {
-			this.YoutubePlayerSettings.queueVideo(video);
-			this.YoutubePlayerSettings.playVideoId(video);
-		}
+    playVideo(video) {
+      this.YoutubePlayerSettings.queueVideo(video);
+      this.YoutubePlayerSettings.playVideoId(video);
+    }
 
-		playPlaylist(playlist) {
-			return this.PlaylistInfo.list(playlist.id).then(this.YoutubePlayerSettings
-				.playPlaylist);
-		}
+    playPlaylist(playlist) {
+      return this.PlaylistInfo.list(playlist.id).then(this.YoutubePlayerSettings
+        .playPlaylist);
+    }
 
-		addVideo(video) {
-			this.PlaylistEditorSettings.add(video);
-			this.$state.go('addVideo', {
-				id: video.id
-			})
-		}
+    addVideo(video) {
+      this.PlaylistEditorSettings.add(video);
+      this.$state.go('addVideo', {
+        id: video.id
+      });
+    }
 
-		queueVideo(video) {
-			this.YoutubePlayerSettings.queueVideo(video);
-		}
+    queueVideo(video) {
+      this.YoutubePlayerSettings.queueVideo(video);
+    }
 
-		toggleFilters() {
-			this.hideFilters = !this.hideFilters;
-		}
-	}
+    toggleFilters() {
+      this.hideFilters = !this.hideFilters;
+    }
+  }
 };
