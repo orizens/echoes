5558340aba4bdc5f57741573b6ac140e2889a21f
Oren Farhi <farhioren@gmail.com>
2016-08-25 14:30:32 +0300

    disabled components test

--- specs.bundle.js
+++ specs.bundle.js
@@ -10,10 +10,10 @@ import mocks from 'angular-mocks';
 // all files ending with `.spec.js` and trace its path.
 // By passing in true, we permit this process to occur recursively.
 let context = require.context('./src/core/', true, /\.spec\.js/);
-let components = require.context('./src/components/youtube-videos/', true, /\.spec\.js/);
+// let components = require.context('./src/components/youtube-videos/', true, /\.spec\.js/);
 
 // Get all files, for each file, call the context function
 // that will require the file and load it here. Context will
 // loop and require those spec files here.
 context.keys().forEach(context);
-components.keys().forEach(components);
+// components.keys().forEach(components);
