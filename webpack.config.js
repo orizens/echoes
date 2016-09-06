ec31431c5d165e8b45cfbea60ba01deff335db14
orizens <farhioren@gmail.com>
2016-09-06 10:56:11 +0300

    updated manifest icon and color

--- webpack.config.js
+++ webpack.config.js
@@ -28,48 +28,46 @@ module.exports = {
   },
   module: {
     loaders: [{
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        loaders: ['babel']
-      }, {
-        test: /\.html$/,
-        loader: 'ngtemplate!html',
-        exclude: /(index)/
-      }, {
-        test: /\.less$/,
-        loader: ExtractTextPlugin.extract('css?sourceMap!' + 'less?sourceMap')
-      },
-      // FONTS
-      {
-        test: /\.woff$/,
-        loader: 'url?limit=100000&name=./fonts/[name]/[hash].[ext]'
-      }, {
-        test: /\.eot$/,
-        loader: 'file'
-      }, {
-        test: /\.svg$/,
-        loader: 'url?limit=100000&name=./fonts/[name]/[hash].[ext]'
-      },
-      // the url-loader uses DataUrls.
-      // the file-loader emits files.
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url?limit=10000&minetype=application/font-woff'
-      }, {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file'
-      }
-
-    ]
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      loaders: ['babel']
+    }, {
+      test: /\.html$/,
+      loader: 'ngtemplate!html',
+      exclude: /(index)/
+    }, {
+      test: /\.less$/,
+      loader: ExtractTextPlugin.extract('css?sourceMap!' + 'less?sourceMap')
+    },
+    // FONTS
+    {
+      test: /\.woff$/,
+      loader: 'url?limit=100000&name=./fonts/[name]/[hash].[ext]'
+    }, {
+      test: /\.eot$/,
+      loader: 'file'
+    }, {
+      test: /\.svg$/,
+      loader: 'url?limit=100000&name=./fonts/[name]/[hash].[ext]'
+    },
+    // the url-loader uses DataUrls.
+    // the file-loader emits files.
+    {
+      test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: 'url?limit=10000&minetype=application/font-woff'
+    }, {
+      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      loader: 'file'
+    }]
   },
 
   plugins: [
     new ngAnnotatePlugin({
       add: true
-        // other ng-annotate options here
+      // other ng-annotate options here
     }),
     new webpack.optimize.CommonsChunkPlugin('vendors',
-      'vendors.[hash].js'),
+    'vendors.[hash].js'),
     new ExtractTextPlugin('[name].[chunkhash].style.css'),
     // HtmlWebpackPlugin
     // See: https://github.com/ampedandwired/html-webpack-plugin
