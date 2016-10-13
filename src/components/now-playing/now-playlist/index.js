d6a302360c03e38ae01e9be387efe56882e759cb
orizens <farhioren@gmail.com>
2016-10-13 11:13:26 +0300

    code cleanup

--- src/components/now-playing/now-playlist/index.js
+++ src/components/now-playing/now-playlist/index.js
@@ -1,13 +1,14 @@
 import angular from 'angular';
+/* eslint-disable */
 import AngularSortableView from 'angular-sortable-view/src/angular-sortable-view.js';
+/* eslint-enable */
 import { NowPlaylistComponent } from './now-playlist.component';
 
 export default angular.module('now-playlist', [
-	    'app.core',
-	    'angular-sortable-view'
-    ])
-    .config(config)
-    .component(NowPlaylistComponent.selector, NowPlaylistComponent)
+  'angular-sortable-view'
+])
+  .config(config)
+  .component(NowPlaylistComponent.selector, NowPlaylistComponent)
 ;
 /* @ngInject */
 function config () {
